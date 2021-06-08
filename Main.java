a6785cd7265bce4b88f35577614685f1c96ba506
DhanyaNagaraja <85445974+DhanyaNagaraja@users.noreply.github.com>
2021-06-08 10:29:49 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ Scanner sc=new Scanner(System.in);
 String str=""; 
 System.out.println("Enter input string :");
 str=sc.nextLine();
-String temp=str.substring(2)+str.substring(0,2);
+String temp=str.substring(n/2,n)+str.substring(0,n/2);
 //System.out.println(temp);
 int i,j;
 for(i=0;i<temp.length();i++)
